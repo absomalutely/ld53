20191ca8453ac89604ad6eab8c56b48535710de3
Will White <willwhite84@gmail.com>
2023-04-29 13:54:31 +0100

    tweaking values

--- Assets/Components/Player/PlayerInput.cs
+++ Assets/Components/Player/PlayerInput.cs
@@ -10,47 +10,68 @@ public class PlayerInput : MonoBehaviour {
     bool faceLeft = true;
     public float movementSpeed = 1000f;
 
-    public float jumpHeight = 100f;
+    public float jumpHeight = 400f;
 
     //bool allowJump = true;
     bool isJumping = false;
     bool isMoving = true;
 
-    Vector2 move;
+    float moveHorizontal = 0f;
+    float moveVertical = 0f;
 
     void Start() {
         playerRB = player.GetComponent<Rigidbody2D>();
         spriteRenderer = player.GetComponent<SpriteRenderer>();
         Debug.Log("Started");
+
+
     }
 
     void Update() {
         HandleAnimator();
 
-        
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Vertical");
+
+        isMoving = Mathf.Abs(moveHorizontal) > 0.1f;
+        if (isMoving) {
+            faceLeft = moveHorizontal < 0f;
+        }
     }
 
     void FixedUpdate() {
-        if (Input.GetKey(KeyCode.D)) {
-            playerRB.velocity = new Vector2(playerRB.position.x + movementSpeed * Time.deltaTime, playerRB.velocity.y);
+        //    if (Input.GetKey(KeyCode.D)) {
+        //        playerRB.velocity = new Vector2(playerRB.position.x + movementSpeed * Time.deltaTime, playerRB.velocity.y);
 
-            //transform.position += Vector3.right * movementSpeed * Time.deltaTime;
-            faceLeft = false;
-            isMoving = true;
+        //        //transform.position += Vector3.right * movementSpeed * Time.deltaTime;
+        //        faceLeft = false;
+        //        isMoving = true;
 
-        } else if (Input.GetKey(KeyCode.A)) {
-            //playerRB.MovePosition(new Vector2(playerRB.position.x - movementSpeed * Time.deltaTime, playerRB.position.y));
-            playerRB.velocity = new Vector2(playerRB.position.x - movementSpeed * Time.deltaTime, playerRB.velocity.y);
-            faceLeft = true;
-            isMoving = true;
-        } else {
-            isMoving = false;
+        //    } else if (Input.GetKey(KeyCode.A)) {
+        //        //playerRB.MovePosition(new Vector2(playerRB.position.x - movementSpeed * Time.deltaTime, playerRB.position.y));
+        //        playerRB.velocity = new Vector2(playerRB.position.x - movementSpeed * Time.deltaTime, playerRB.velocity.y);
+        //        faceLeft = true;
+        //        isMoving = true;
+        //    } else {
+        //        isMoving = false;
+        //    }
+
+        //    if (Input.GetKey(KeyCode.Space)) {
+        //        if (!isJumping) {
+        //            playerRB.AddForce(new Vector2(0, movementSpeed * jumpHeight));
+        //            isJumping = true;
+        //        }
+        //    }aw
+
+        if (moveHorizontal > 0.1f || moveHorizontal < -0.1f) {
+            playerRB.AddForce(new Vector2(moveHorizontal * movementSpeed, 0f), ForceMode2D.Impulse);
         }
 
-        if (Input.GetKey(KeyCode.Space)) {
+        if (moveVertical > 0.1f || moveVertical < -0.1f) {
             if (!isJumping) {
-                playerRB.AddForce(new Vector2(0, movementSpeed * jumpHeight));
+                playerRB.AddForce(new Vector2(0f, moveVertical * jumpHeight), ForceMode2D.Impulse);
                 isJumping = true;
+                Debug.Log("Jumping: " + moveVertical);
             }
         }
     }
